02ce59491dfa136dbdb78e4dd2d2133a097a659f
David Larlet <david@larlet.fr>
2017-10-03 10:42:16 -0400

    Add a password field, refs #22

--- core/models.py
+++ core/models.py
@@ -1,21 +1,24 @@
 import datetime
 
-from peewee import Model, CharField, DateField
-
 from core import db
+from peewee import CharField, DateField, Model
+from playhouse.fields import PasswordField
 
 
 class User(Model):
     email = CharField(unique=True)
     firstname = CharField()
     lastname = CharField()
+    password = PasswordField()
     creation_date = DateField(default=datetime.datetime.utcnow)
 
     def __str__(self):
         return f'{self.firstname} {self.lastname}'
 
     def to_dict(self):
-        return self._data
+        user_dict = self._data.copy()
+        del user_dict['password']
+        return user_dict
 
     class Meta:
         database = db
