bc384cebd61f432e6306dad4feaeb5ca38b76ad8
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-10-03 21:14:23 +0200

    added duplicate emails managment

--- api/views.py
+++ api/views.py
@@ -6,12 +6,30 @@ from core.models import User
 
 @app.route('/api/user/', methods=['post'])
 def api_user_post():
-    user = User.create(**request.get_json())
+    data = request.get_json()
+    try:
+        User.get(email=data['email'])
+        return (jsonify({'error': 'A user with this email already exists.'}),
+                400)
+    except User.DoesNotExist:
+        pass
+    user = User.create(**data)
     return jsonify(user.to_dict()), 201
 
 
 @app.route('/api/user/<int:id>/', methods=['put'])
 def api_user_put(id):
+    try:
+        user = User.get(id=id)
+    except User.DoesNotExist:
+        return {'error': 'User not found.'}, 404
+    data = request.get_json()
+    try:
+        User.select().where(User.id != id, User.email == data['email']).get()
+        return (jsonify({'error': 'A user with this email already exists.'}),
+                400)
+    except User.DoesNotExist:
+        pass
     User.update(**request.get_json()).where(User.id == id).execute()
     user = User.get(id=id)
     return jsonify(user.to_dict())
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,22 +1,75 @@
+/* global fetch */
+// Inspired by https://github.com/zellwk/zl-fetch
+// See https://css-tricks.com/using-fetch/
+function request (url, options = undefined) {
+  return fetch(url, optionsHandler(options))
+    .then(handleResponse)
+    .catch(error => {
+        const e = new Error(`${error.message} ${url}`)
+        Object.assign(e, error, {url})
+        throw e
+      })
+}
 
-  function request (url, params) {
-    params.headers = params.headers || {}
-    params.headers['Content-Type'] = 'application/json'
-    params.body = params.body && JSON.stringify(params.body)
-    const response = {}
-
-    return fetch(url, params)
-    .then(r => {
-      if (!r.ok) {
-        console.error(url, r)
-        throw new Error(url)
-      }
-      response.status = r.status
-      // `r.json()` crashes if the body is empty.
-      return r.text()
-    })
-    .then(body => {
-      response.body = body && JSON.parse(body)
-      return response
-    })
+function optionsHandler (options) {
+  const def = {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
   }
+
+  if (!options) return def
+
+  let r = Object.assign({}, def, options)
+
+  // Deal with body, can be either a hash or a FormData,
+  // will generate a JSON string from it if in options.
+  delete r.body
+  if (options.body) {
+    // Allow to pass an empty hash too.
+    if (!(Object.getOwnPropertyNames(options.body).length === 0)) {
+      r.body = JSON.stringify(options.body)
+    } else if (options.body instanceof FormData) {
+      r.body = JSON.stringify(Array.from(options.body.entries()))
+    }
+  }
+  return r
+}
+
+const handlers = {
+  JSONResponseHandler (response) {
+    return response.json()
+      .then(json => {
+        if (response.ok) {
+          return json
+        } else {
+          return Promise.reject(Object.assign({}, json, {
+            status: response.status
+          }))
+        }
+      })
+  },
+  textResponseHandler (response) {
+    if (response.ok) {
+      return response.text()
+    } else {
+      return Promise.reject(Object.assign({}, {
+        status: response.status,
+        message: response.statusText
+      }))
+    }
+  }
+}
+
+function handleResponse (response) {
+  let contentType = response.headers.get('content-type')
+  if (contentType.includes('application/json')) {
+    return handlers.JSONResponseHandler(response)
+  } else if (contentType.includes('text/html')) {
+    return handlers.textResponseHandler(response)
+  } else {
+    throw new Error(`
+      Sorry, content-type '${contentType}' is not supported,
+      only 'application/json' and 'text/html' are.
+    `)
+  }
+}
