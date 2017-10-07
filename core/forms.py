83b49cdd84d9f7121d37c38f58ef977df451806e
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-10-07 17:49:40 -0400

    removed user profile forms

--- core/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email
-
-
-class UserForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email()])
-    firstname = StringField('firstname', validators=[DataRequired()])
-    firstname = StringField('firstname', validators=[DataRequired()])
