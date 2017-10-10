d71771f19fda260ce0cb186570d270882736e6a8
Vincent Agnano <vincent.agnano@scopyleft.fr>
2017-10-10 15:57:31 -0400

    saving profile with flask security

--- cafebabel/core/__init__.py
+++ cafebabel/core/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
-from flask_peewee.db import Database
+from .database import Database
 from flask_mail import Mail
 
 
@@ -10,7 +10,6 @@ app = Flask(__name__)
 ROOT_PATH = Path(__file__).parent.parent.parent
 app.root_path = str(ROOT_PATH)
 app.config.from_pyfile(str(ROOT_PATH / 'settings.py'))
-
 db = Database(app)
 mail = Mail(app)
 toolbar = DebugToolbarExtension(app)
