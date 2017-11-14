f2e1624b4a2297ce1ed7dd8a8b8364de1fc07391
vinyll <vincent.agnano@scopyleft.fr>
2017-11-14 19:54:48 +0100

    published articles are visible in the author's page

--- cafebabel/users/views.py
+++ cafebabel/users/views.py
@@ -1,6 +1,7 @@
 from flask import abort, render_template
 from flask_login import login_required, current_user
 
+from ..articles.models import Article
 from .. import app
 from .models import User
 
@@ -18,4 +19,6 @@ def profile_user(id):
         user = User.objects.get(id=id)
     except User.DoesNotExist:
         abort(404, 'User not found.')
-    return render_template('profile.html', user=user, edit=False)
+    articles = Article.objects.filter(author=user, status='published')
+    return render_template('profile.html', user=user, articles=articles,
+                           edit=False)
