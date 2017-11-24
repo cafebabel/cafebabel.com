2863fcc70f748bf99bcae4ade55d8405584d328f
cafebabel-webmaster <webmaster@cafebabel.com>
2017-11-24 12:29:04 +0100

    flash info pop up animation + template adaptation on to-translate list page

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,9 @@ menuButton.addEventListener('click', () => {
 Array.from(document.querySelectorAll('a')).forEach(a => {
     if (a.href.search(/\w+:\/\//) === 0 && a.hostname !== location.hostname) a.setAttribute('target', '_blank')
 })
+
+/* animation flash info */
+
+setTimeout(() => {
+    document.querySelector('.flashes').classList.add('hidden');
+}, 3000);
