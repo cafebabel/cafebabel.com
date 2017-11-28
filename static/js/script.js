4afafbded9ce979742b03c887834a8e38ac25cf6
cafebabel-webmaster <webmaster@cafebabel.com>
2017-11-28 17:52:30 +0100

    correction preloader page class => id

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,17 @@
 /* Preloader fadeout when page is loaded */
 window.onload = function() {
-    const preloader = document.getElementsByClassName('preloader')[0];
-    preloader.className += ' fadeout';
+    const preloader = document.querySelector('#preloader');
+    preloader.classList.add('fadeout');
     setTimeout(function(){
         preloader.style.display = 'none';
     }, 300);
-}; /* There might be a simplier way to do it, I just got help from here : https://stackoverflow.com/questions/19838955/jquery-loading-screen-into-pure-javascript */
+} /* There might be a simplier way to do it, I just got help from here : https://stackoverflow.com/questions/19838955/jquery-loading-screen-into-pure-javascript */
 
 /* open menu */
 const menuButton = document.querySelector('button.menu-button');
 menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('active');
-});
+    menuButton.classList.toggle('active')
+})
 
 /* external link in new tab */
 Array.from(document.querySelectorAll('a')).forEach(a => {
@@ -20,25 +20,25 @@ Array.from(document.querySelectorAll('a')).forEach(a => {
 
 /* animation flash info */
 setTimeout(() => {
-    document.querySelector('.flashes').classList.add('hidden');
-}, 3000);
+    document.querySelector('.flashes').classList.add('hidden')
+}, 3000)
 
 /* display profile social network field on click */
 const socialIcon = document.querySelector('.social-networks li');
 socialIcon.addEventListener('click', (event) => {
     event.preventDefault();
-    this.document.querySelector('label').classList.toggle('active');
+    this.document.querySelector('label').classList.toggle('active')
 });
 
 /* animation logo fields */
 const loginField = document.querySelector('.text input');
 loginField.addEventListener('focus', event => {
     console.log('focus')
-    event.target.parent.classList.add('active', 'complete');
-});
+    event.target.parent.classList.add('active', 'complete')
+})
 loginField.addEventListener('blur', event => {
     if(event.target.value === '') {
-    event.target.classList.remove('completed', 'active');
+    event.target.classList.remove('completed', 'active')
     }
 })
 
