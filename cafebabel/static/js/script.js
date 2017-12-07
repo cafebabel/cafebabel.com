b9e25c9aeb8d57ffb7d27f4fdb2626176d03c0a2
cafebabel-webmaster <webmaster@cafebabel.com>
2017-12-07 16:23:53 +0100

    trying tv noise canvas + title placeholder

--- cafebabel/static/js/script.js
+++ cafebabel/static/js/script.js
@@ -62,13 +62,50 @@ function addListenerMulti(element, events, fn) {
     events.split(' ').forEach(event => element.addEventListener(event, fn, false)) /* https://stackoverflow.com/questions/8796988/binding-multiple-events-to-a-listener-without-jquery */
 }
 const fileInput = document.querySelector('.file input')
-const dropArea = document.querySelector('.file')
-addListenerMulti(fileInput, 'dragenter focus click', () => {
-    dropArea.classList.add('active')
-})
-addListenerMulti(fileInput, 'dragleave blur drop', () => {
-    dropArea.classList.remove('active')
-})
-fileInput.addEventListener('change', () => {
-    document.querySelector('.file label').innerHTML(' ')
-})
+const dropArea = document.querySelector('canvas')
+if (fileInput) {
+    addListenerMulti(fileInput, 'dragenter focus click', () => {
+        dropArea.classList.add('active')
+    })
+    addListenerMulti(fileInput, 'dragleave blur drop', () => {
+        dropArea.classList.remove('active')
+    })
+    fileInput.addEventListener('change', () => {
+        document.querySelector('.file label').innerHTML(' ')
+    })
+}
+
+/* tv noise effect https://codepen.io/eugene-bulkin/pen/zEgyH how to know how "heavy" is it? */
+const canvas = document.querySelector('canvas')
+if (canvas) {
+    const context = canvas.getContext('2d')
+
+    function resize() {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+    }
+    resize();
+    window.onresize = resize
+
+    function noise(context) {
+        var w = context.canvas.width,
+            h = context.canvas.height,
+            idata = context.createImageData(w, h),
+            buffer32 = new Uint32Array(idata.data.buffer),
+            len = buffer32.length,
+            i = 0
+        for(; i < len;)
+            buffer32[i++] = ((50 * Math.random())|0) << 24
+        context.putImageData(idata, 0, 0)
+    }
+    var toggle = true;
+    (function loop() {
+        toggle = !toggle
+        if (toggle) {
+            requestAnimationFrame(loop)
+            return
+        }
+        noise(context)
+        requestAnimationFrame(loop)
+    })();
+}
