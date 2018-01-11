5267fc4b2a0c663a6f4ff1fac90826a172af9da8
David Larlet <david@larlet.fr>
2018-01-10 21:38:28 -0500

    Include tags in article detail page

--- cafebabel/tests/test_articles.py
+++ cafebabel/tests/test_articles.py
@@ -305,3 +305,13 @@ def test_article_with_tags(app, tag, article):
     tag2.delete()
     article.reload()
     assert article.tags == [tag]
+
+
+def test_article_detail_contains_tags(client, app, tag, published_article):
+    language = app.config['LANGUAGES'][0][0]
+    tag2 = Tag.objects.create(name='Tag two', language=language)
+    published_article.modify(tags=[tag, tag2])
+    response = client.get(
+        f'/article/{published_article.slug}-{published_article.id}/')
+    assert response.status_code == HTTPStatus.OK
+    assert 'A tag, Tag two.' in response
