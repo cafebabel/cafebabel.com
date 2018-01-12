31aa378da869e47402b27ad9dc7d1b300b48f087
cafebabel-webmaster <webmaster@cafebabel.com>
2018-01-12 17:15:38 +0100

    add js file + wip display more input

---
+++ cafebabel/static/js/edit-article.js
@@ -0,0 +1,6 @@
+const tagButtonAdd = document.querySelector('.tags button.add')
+const tagButtonRemove = document.querySelector('.tags button.remove')
+tagButtonAdd.addEventListener('click', (event) => {
+  event.preventDefault()
+})
+
