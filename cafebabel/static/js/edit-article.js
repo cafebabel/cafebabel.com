5d013792c681c0721850376f01fd108222c8d5c0
cafebabel-webmaster <webmaster@cafebabel.com>
2018-01-15 15:10:29 +0100

    Refactor with class

--- cafebabel/static/js/edit-article.js
+++ cafebabel/static/js/edit-article.js
@@ -1,36 +1,48 @@
-const tagButtonAdd = document.querySelector('.tags button.add')
-const tagButtonRemove = document.querySelector('.tags button.remove')
-const tagsList = document.querySelector('.tags .tagsList')
+class Tags {
+  constructor(domContext) {
+    this.list = domContext.querySelector('ul')
+    this.values = Array.from(this.list.querySelectorAll('li')).map(li => li.innerText)
+    this.submission = domContext.querySelector('input[name=tag-new]').value
+  }
+  checkSubmission() {
+    return this.submission && ! this._isTag(this.submission)
+  }
+  display() {
+    const tagsValue = this.values.concat(this.submission)
+    const container = this.list.cloneNode(false)
+    this.list.replaceWith(this._createTagsList(container, tagsValue))
+  }
 
-const Tag = {
-  count: () => tagsList.querySelectorAll('li').length,
-  createTag(tagValue) {
-    const li = document.createElement('li')
-    const input = document.createElement('input')
-    li.append(tagValue)
-    input.type = 'hidden'
-    input.name = `tag-${Tag.count()}`
-    input.setAttribute('list', 'tags')
-    input.value = tagValue
-    li.append(input)
-
-    return li
-  },
-  tagValues: () => Array.from(tagsList.querySelectorAll('li')).map(li => li.innerText),
-  createTagsList: (container, tagValues) => {
-    tagValues.forEach(tagValue => container.appendChild(Tag.createTag(tagValue)))
+  _isTag(tag) {
+    return this.values.includes(tag)
+  }
+  _createTagsList(container, tagValues) {
+    tagValues.forEach((tagValue, index) => container.appendChild(this._createTag(tagValue, index)))
 
     return container
   }
+  _createTag(tagValue, index) {
+    const li = document.createElement('li')
+    const input = document.createElement('input')
+    input.type = 'hidden'
+    input.name = `tag-${index + 1}`
+    input.setAttribute('list', 'tags')
+    input.value = tagValue
+    li.append(tagValue)
+    li.append(input)
+
+    return li
+  }
 }
+
+
+const tagButtonAdd = document.querySelector('.tags button.add')
+const tagButtonRemove = document.querySelector('.tags button.remove')
 
 tagButtonAdd.addEventListener('click', (event) => {
   event.preventDefault()
-  const tag = event.target.previousElementSibling.value
-  if (!tag || Tag.tagValues().includes(tag)) return
-  const tags = Tag.tagValues().concat(tag)
-  const container = tagsList.cloneNode(false)
-  tagsList.innerHTML = ''
-  tagsList.append(Tag.createTagsList(container, tags))
+  const tags = new Tags(document.querySelector('.tags'))
+  if (!tags.checkSubmission()) return
+  tags.display()
 })
 
