fcf1933a20633f6076284cac55a1602453747206
pntbr <stephane.langlois@scopyleft.fr>
2018-01-15 11:10:23 +0100

    Get tag's value

--- cafebabel/static/js/edit-article.js
+++ cafebabel/static/js/edit-article.js
@@ -2,5 +2,10 @@ const tagButtonAdd = document.querySelector('.tags button.add')
 const tagButtonRemove = document.querySelector('.tags button.remove')
 tagButtonAdd.addEventListener('click', (event) => {
   event.preventDefault()
+  const tag = event.target.previousElementSibling.value
+  if (!tag) {
+    return
+  }
+  console.log('Tag', tag)
 })
 
