b09ad671b62d6ad6807137d18ba136a41f37cff3
cafebabel-webmaster <webmaster@cafebabel.com>
2018-01-16 19:52:53 +0100

    Refactor: move function to class

--- cafebabel/static/js/edit-article.js
+++ cafebabel/static/js/edit-article.js
@@ -1,8 +1,15 @@
 class Tags {
-  constructor(domContext) {
-    this.list = domContext.querySelector('.tags .tags-list')
+  constructor() {
+    const domContext = document.querySelector('.tags')
+    this.list = domContext.querySelector('.tags-list')
     this.values = Array.from(this.list.querySelectorAll('input')).map(
       input => input.value
+    )
+  }
+  static addTagsRemoveListener() {
+    const tagsButtonRemove = document.querySelectorAll('.tags .tags-list li a')
+    tagsButtonRemove.forEach(tagButtonRemove =>
+      Listener.clickRemove(tagButtonRemove)
     )
   }
   checkSubmission(query) {
@@ -13,6 +20,11 @@ class Tags {
   }
   remove(query) {
     this.values = this.values.filter(value => value !== query)
+  }
+  display() {
+    const container = this.list.cloneNode(false)
+    this.list.replaceWith(this._createTagsList(container, this.values))
+    Tags.addTagsRemoveListener()
   }
   _isTag(tag) {
     return this.values.includes(tag)
@@ -40,72 +52,6 @@ class Tags {
   }
 }
 
-const tagButtonAdd = document.querySelector('.tags button.add')
-
-tagButtonAdd.addEventListener('click', event => {
-  event.preventDefault()
-  const submission = event.target.previousSibling.value
-  const tags = new Tags(document.querySelector('.tags'))
-  if (tags.checkSubmission(submission)) tags.add(submission)
-  displayTags(tags)
-  Effect.fadeIn(document.querySelector('.tags .tags-list li:last-child'))
-  document.querySelector('.tags input[name=tag-new]').value = ''
-})
-
-function handleSuggestion(tag) {
-  const tagsSuggestion = document.querySelector('#tags-suggestions')
-  tagsSuggestion.innerHTML = ''
-  const li = document.createElement('li')
-  li.append(tag.name)
-  tagsSuggestion.appendChild(li)
-  tagsSuggestion.classList.remove('inactive')
-  tagsSuggestion.addEventListener('click', event => {
-    const submission = event.target.innerText
-    const tags = new Tags(document.querySelector('.tags'))
-    if (tags.checkSubmission(submission)) tags.add(submission)
-    displayTags(tags)
-    li.remove()
-    document.querySelector('.tags input[name=tag-new]').value = ''
-    tagsSuggestion.classList.add('inactive')
-  })
-}
-
-document
-  .querySelector('.tags input[name=tag-new]')
-  .addEventListener('keyup', event => {
-    /* Return if arrow up, arrow down or enter are pressed */
-    if (event.keyCode == 40 || event.keyCode == 38 || event.keyCode == 13)
-      return
-    document.querySelector('#tags-suggestions').innerHTML = ''
-    const submission = event.target.value
-    const languages = document.querySelector('#language')
-    const language = languages.options[languages.selectedIndex].value
-    if (submission.length < 3) return
-    request(`/article/tag/suggest/?language=${language}&terms=${submission}`)
-      .then(response => response.forEach(handleSuggestion))
-      .catch(console.error.bind(console))
-  })
-
-function addTagsRemoveListener() {
-  const tagsButtonRemove = document.querySelectorAll('.tags .tags-list li a')
-  tagsButtonRemove.forEach(tagButtonRemove =>
-    tagButtonRemove.addEventListener('click', event => {
-      const tags = new Tags(document.querySelector('.tags'))
-      const tagElement = event.target.parentNode
-      tags.remove(tagElement.innerText)
-      Effect.fadeOut(tagElement).then(response => displayTags(tags))
-    })
-  )
-}
-
-window.addEventListener('load', addTagsRemoveListener)
-
-function displayTags(tags) {
-  const container = tags.list.cloneNode(false)
-  tags.list.replaceWith(tags._createTagsList(container, tags.values))
-  addTagsRemoveListener()
-}
-
 class Effect {
   static fadeIn(element) {
     element.style.opacity = 0
@@ -131,3 +77,70 @@ class Effect {
     })
   }
 }
+
+class Listener {
+  static clickAdd() {
+    const tagButtonAdd = document.querySelector('.tags button.add')
+
+    tagButtonAdd.addEventListener('click', event => {
+      event.preventDefault()
+      const submission = event.target.previousSibling.value
+      const tags = new Tags()
+      if (tags.checkSubmission(submission)) tags.add(submission)
+      tags.display()
+      Effect.fadeIn(document.querySelector('.tags .tags-list li:last-child'))
+      document.querySelector('.tags input[name=tag-new]').value = ''
+    })
+  }
+  static clickRemove(tagButtonRemove) {
+    tagButtonRemove.addEventListener('click', event => {
+      const tags = new Tags()
+      const tagElement = event.target.parentNode
+      tags.remove(tagElement.innerText)
+      Effect.fadeOut(tagElement).then(response => tags.display())
+    })
+  }
+
+  static keyup() {
+    function handleSuggestion(tag) {
+      const tagsSuggestion = document.querySelector('#tags-suggestions')
+      tagsSuggestion.innerHTML = ''
+      const li = document.createElement('li')
+      li.append(tag.name)
+      tagsSuggestion.appendChild(li)
+      tagsSuggestion.classList.remove('inactive')
+      tagsSuggestion.addEventListener('click', event => {
+        const submission = event.target.innerText
+        const tags = new Tags()
+        if (tags.checkSubmission(submission)) tags.add(submission)
+        tags.display()
+        li.remove()
+        document.querySelector('.tags input[name=tag-new]').value = ''
+        tagsSuggestion.classList.add('inactive')
+      })
+    }
+
+    const inputNewTag = document.querySelector('.tags input[name=tag-new]')
+
+    inputNewTag.addEventListener('keyup', event => {
+      /* Return if arrow up, arrow down or enter are pressed */
+      if (event.keyCode == 40 || event.keyCode == 38 || event.keyCode == 13)
+        return
+      const submission = event.target.value
+      const languages = document.querySelector('#language')
+      const language = languages.options[languages.selectedIndex].value
+      if (submission.length < 3) return
+      request(`/article/tag/suggest/?language=${language}&terms=${submission}`)
+        .then(response => response.forEach(handleSuggestion))
+        .catch(console.error.bind(console))
+    })
+  }
+}
+
+Listener.clickAdd()
+Listener.keyup()
+
+window.addEventListener('load', () => {
+  const tags = new Tags(document.querySelector('.tags'))
+  Tags.addTagsRemoveListener()
+})
