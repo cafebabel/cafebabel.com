1013edcb6659461194e31650e3a54de80b483dcd
cafebabel-webmaster <webmaster@cafebabel.com>
2018-01-31 17:13:04 +0100

    fix homepage test

--- cafebabel/tests/test_base.py
+++ cafebabel/tests/test_base.py
@@ -2,7 +2,7 @@
 def test_homepage_is_displaying(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert ('<meta name=description content="pan-European news, features and '
-            'opinions">' in response)
+    assert ('<meta name=description content="Cafébabel is the first European participatory magazine made for young '
+            'Europeans across borders.">' in response)
     assert '<meta property=og:url content="http://localhost/">' in response
     assert '<meta name=twitter:card content="summary">' in response
