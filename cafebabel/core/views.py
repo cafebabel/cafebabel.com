09253c81ddb8e3404a7a9b255381ecd360b7eb01
vinyll <vincent.agnano@scopyleft.fr>
2018-02-06 16:57:06 -0600

    redirecting archived articles from Django to new version

--- cafebabel/core/views.py
+++ cafebabel/core/views.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, current_app, render_template, send_from_directory
+from flask import (Blueprint, current_app, render_template,
+                   send_from_directory, request, redirect, url_for)
+from http import HTTPStatus
+
+from ..articles.models import Article
+from ..articles.translations.models import Translation
+
 
 cores = Blueprint('cores', __name__)
 
@@ -11,3 +17,11 @@ def home():
 @cores.route('/uploads/<path:filename>')
 def uploads(filename):
     return send_from_directory(current_app.config['UPLOADS_FOLDER'], filename)
+
+
+@cores.route('/<regex("[a-z-]+"):_tag>/<regex("[a-z]+"):_>/<_slug>.html')
+def archive(**kwargs):
+    article = Article.objects.get_or_404(archive__url=request.url)
+    return redirect(url_for('articles.detail', slug=article.slug,
+                            article_id=article.id),
+                    HTTPStatus.MOVED_PERMANENTLY)
---
+++ cafebabel/tests/test_articles_archives.py
@@ -0,0 +1,32 @@
+from flask import url_for
+
+from cafebabel.articles.models import ArticleArchive
+
+
+def test_archive_is_redirect_to_artile(client, published_article):
+    url = 'http://localhost/lifestyle/article/old-artile.html'
+    published_article.archive = ArticleArchive(id=1, url=url)
+    published_article.save()
+    response = client.get(url)
+    assert response.status_code == 301
+    article_url = url_for('articles.detail', slug=published_article.slug,
+                          article_id=published_article.id, _external=True)
+    assert response.location == article_url
+
+
+def test_archive_is_redirect_to_translation(client, translation):
+    url = 'http://localhost/lifestyle/articolo/old-translation.html'
+    translation.status = 'published'
+    translation.archive = ArticleArchive(id=1, url=url)
+    translation.save()
+    response = client.get(url)
+    assert response.status_code == 301
+    article_url = url_for('articles.detail', slug=translation.slug,
+                          article_id=translation.id, _external=True)
+    assert response.location == article_url
+
+
+def test_archive_inexisting_renders_404(client, published_article):
+    url = 'http://localhost/lifestyle/article/non-existing-archive.html'
+    response = client.get(url)
+    assert response.status_code == 404
