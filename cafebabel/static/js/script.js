c402800a9405e347c9f4901b5f5acb37be79c4e9
cafebabel-webmaster <webmaster@cafebabel.com>
2018-02-08 12:24:36 +0100

    auto-expand textarea when removing text + keep height when saving article

--- cafebabel/static/js/script.js
+++ cafebabel/static/js/script.js
@@ -114,10 +114,37 @@ Array.from(document.querySelectorAll('h1.edit input')).forEach(inputh1 => {
   )
 })
 
-/* Auto-expand textarea when typing */
-Array.from(document.querySelectorAll('textarea')).forEach(textArea => {
-  textArea.addEventListener('keyup', () => {
-    textArea.style.height = 0
-    textArea.style.height = `${textArea.scrollHeight}px`
+/* Auto-expand textarea when typing http://jsfiddle.net/hmelenok/WM6Gq/ */
+var textAreaAutoResize
+if (window.attachEvent) {
+  textAreaAutoResize = function(element, event, handler) {
+    element.attachEvent('on' + event, handler)
+  }
+} else {
+  textAreaAutoResize = function(element, event, handler) {
+    element.addEventListener(event, handler, false)
+  }
+}
+function textAreaResizing() {
+  Array.from(document.querySelectorAll('textarea')).forEach(textArea => {
+    function resize() {
+      textArea.style.height = 'auto'
+      textArea.style.height = `${textArea.scrollHeight}px`
+    }
+
+    /* 0-timeout to get the already changed text */
+    function delayedResize() {
+      window.setTimeout(resize, 0)
+    }
+
+    textAreaAutoResize(textArea, 'change', resize)
+    textAreaAutoResize(textArea, 'cut', delayedResize)
+    textAreaAutoResize(textArea, 'paste', delayedResize)
+    textAreaAutoResize(textArea, 'drop', delayedResize)
+    textAreaAutoResize(textArea, 'keydown', delayedResize)
+
+    resize()
   })
-})
+}
+
+textAreaResizing()
