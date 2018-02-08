509c8d6d0b2b5839eea5be5847a89a0ed2ff87dd
David Larlet <david@larlet.fr>
2018-02-07 21:14:30 -0500

    Deal with redirections in preproduction, fix #217

--- cafebabel/archives/views.py
+++ cafebabel/archives/views.py
@@ -9,7 +9,15 @@ archives = Blueprint('archives', __name__)
 
 @archives.route('/<regex("[a-z-]+"):_tag>/<regex("[a-z]+"):_>/<_slug>.html')
 def archive(**kwargs):
-    article = Article.objects.get_or_404(archive__url=request.url)
+    # First deal with regular production redirections,
+    # then fallback on redirections for preproduction.
+    try:
+        article = Article.objects.get(archive__url=request.url)
+    except Article.DoesNotExist:
+        # Only in use for preproduction testing.
+        archive_url = request.url.replace('http://preprod.cafebabel.',
+                                          'http://www.cafebabel.')
+        article = Article.objects.get_or_404(archive__url=archive_url)
     return redirect(url_for('articles.detail', slug=article.slug,
                             article_id=article.id),
                     HTTPStatus.MOVED_PERMANENTLY)
--- cafebabel/tests/test_articles_archives.py
+++ cafebabel/tests/test_articles_archives.py
@@ -5,11 +5,22 @@ from flask import url_for
 from cafebabel.articles.models import ArticleArchive
 
 
-def test_archive_is_redirect_to_artile(client, published_article):
-    url = 'http://localhost/lifestyle/article/old-artile.html'
-    published_article.archive = ArticleArchive(id=1, url=url)
-    published_article.save()
+def test_archive_is_redirect_to_article(client, published_article):
+    url = 'http://localhost/lifestyle/article/old-article.html'
+    published_article.modify(archive=ArticleArchive(id=1, url=url))
     response = client.get(url)
+    assert response.status_code == HTTPStatus.MOVED_PERMANENTLY
+    article_url = url_for('articles.detail', slug=published_article.slug,
+                          article_id=published_article.id, _external=True)
+    assert response.location == article_url
+
+
+def test_archive_is_redirect_from_production(client, published_article):
+    url_prod = 'http://www.cafebabel.co.uk/lifestyle/article/old-article.html'
+    url_preprod = ('http://preprod.cafebabel.co.uk/lifestyle/article/'
+                   'old-article.html')
+    published_article.modify(archive=ArticleArchive(id=1, url=url_prod))
+    response = client.get(url_preprod)
     assert response.status_code == HTTPStatus.MOVED_PERMANENTLY
     article_url = url_for('articles.detail', slug=published_article.slug,
                           article_id=published_article.id, _external=True)
@@ -19,8 +30,7 @@ def test_archive_is_redirect_to_artile(client, published_article):
 def test_archive_is_redirect_to_translation(client, translation):
     url = 'http://localhost/lifestyle/articolo/old-translation.html'
     translation.status = 'published'
-    translation.archive = ArticleArchive(id=1, url=url)
-    translation.save()
+    translation.modify(archive=ArticleArchive(id=1, url=url))
     response = client.get(url)
     assert response.status_code == HTTPStatus.MOVED_PERMANENTLY
     article_url = url_for('articles.detail', slug=translation.slug,
