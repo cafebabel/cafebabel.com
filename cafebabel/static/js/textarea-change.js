10bb3963d1b564d3960cf9d6de02a86aa9500a9b
David Larlet <david@larlet.fr>
2018-02-21 11:54:59 -0500

    Simplify auto-expand textarea

--- cafebabel/static/js/textarea-change.js
+++ cafebabel/static/js/textarea-change.js
@@ -1,27 +1,19 @@
 /* Auto-expand textarea when typing http://jsfiddle.net/hmelenok/WM6Gq/ */
-function textAreaAutoResize(element, event, handler) {
-  element.addEventListener(event, handler, false)
-}
+Array.from(document.querySelectorAll('textarea')).forEach(textArea => {
+  function resize() {
+    textArea.style.height = `${textArea.scrollHeight}px`
+  }
 
-function textAreaResizing() {
-  Array.from(document.querySelectorAll('textarea')).forEach(textArea => {
-    function resize() {
-      textArea.style.height = `${textArea.scrollHeight}px`
-    }
+  /* 0-timeout to get the already changed text */
+  function delayedResize() {
+    window.setTimeout(resize, 0)
+  }
 
-    /* 0-timeout to get the already changed text */
-    function delayedResize() {
-      window.setTimeout(resize, 0)
-    }
+  textArea.addEventListener('change', resize, false)
+  textArea.addEventListener('cut', delayedResize, false)
+  textArea.addEventListener('paste', delayedResize, false)
+  textArea.addEventListener('drop', delayedResize, false)
+  textArea.addEventListener('keydown', delayedResize, false)
 
-    textAreaAutoResize(textArea, 'change', resize)
-    textAreaAutoResize(textArea, 'cut', delayedResize)
-    textAreaAutoResize(textArea, 'paste', delayedResize)
-    textAreaAutoResize(textArea, 'drop', delayedResize)
-    textAreaAutoResize(textArea, 'keydown', delayedResize)
-
-    resize()
-  })
-}
-
-textAreaResizing()
+  resize()
+})
