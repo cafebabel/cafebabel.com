566bd93f43cc6624d3e58cda253b2d1260a44c8f
cafebabel-webmaster <webmaster@cafebabel.com>
2018-03-15 17:41:31 +0100

    article/edit changes

--- cafebabel/tests/test_articles.py
+++ cafebabel/tests/test_articles.py
@@ -136,7 +136,7 @@ def test_update_published_article_should_return_200(app, client, user, editor,
     response = client.post(f'/en/article/{published_article.id}/edit/',
                            data=data, follow_redirects=True)
     assert response.status_code == HTTPStatus.OK
-    assert get_flashed_messages() == ['Your article was successfully updated.']
+    assert get_flashed_messages() == ['Your article has been updated']
     published_article.reload()
     assert published_article.title == 'updated'
     assert published_article.authors == [user]
@@ -152,7 +152,7 @@ def test_update_published_article_with_many_authors(app, client, user, user2,
     response = client.post(f'/en/article/{published_article.id}/edit/',
                            data=data, follow_redirects=True)
     assert response.status_code == HTTPStatus.OK
-    assert get_flashed_messages() == ['Your article was successfully updated.']
+    assert get_flashed_messages() == ['Your article has been updated']
     published_article.reload()
     assert published_article.authors == [user, user2]
 
@@ -183,7 +183,7 @@ def test_update_published_article_with_tag(app, client, user, editor, tag,
     response = client.post(f'/en/article/{published_article.id}/edit/',
                            data=data, follow_redirects=True)
     assert response.status_code == HTTPStatus.OK
-    assert get_flashed_messages() == ['Your article was successfully updated.']
+    assert get_flashed_messages() == ['Your article has been updated']
     published_article.reload()
     assert published_article.tags == [tag]
 
@@ -202,7 +202,7 @@ def test_update_published_article_with_unkown_tag(app, client, user, editor,
     response = client.post(f'/en/article/{published_article.id}/edit/',
                            data=data, follow_redirects=True)
     assert response.status_code == HTTPStatus.OK
-    assert get_flashed_messages() == ['Your article was successfully updated.']
+    assert get_flashed_messages() == ['Your article has been updated']
     published_article.reload()
     tag2 = Tag.objects.get(name='Sensational', language=language)
     assert published_article.tags == [tag, tag2]
@@ -222,7 +222,7 @@ def test_update_article_with_image_should_return_200(app, client, user, editor,
                            data=data, content_type='multipart/form-data',
                            follow_redirects=True)
     assert response.status_code == HTTPStatus.OK
-    assert get_flashed_messages() == ['Your article was successfully updated.']
+    assert get_flashed_messages() == ['Your article has been updated']
     published_article.reload()
     assert published_article.title == 'updated'
     assert published_article.authors == [user]
