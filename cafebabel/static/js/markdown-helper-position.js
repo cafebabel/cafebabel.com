60587db7237f76c7d9f86ec1f08eee3fc939a836
cafebabel-webmaster <webmaster@cafebabel.com>
2018-03-30 13:20:50 +0200

    add markdown-helper a fixed position when body text textarea is in viewport

---
+++ cafebabel/static/js/markdown-helper-position.js
@@ -0,0 +1,36 @@
+/* Add fixed position to markdown helper */
+const markdownHelper = document.querySelector('#markdown-helper')
+
+const editPageTextArea = document.querySelector('.textarea.bodytext'),
+  editPageTextAreaHeight = editPageTextArea.offsetHeight,
+  viewportHeight = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  ),
+  markdownHelperCross = markdownHelper.querySelector('button')
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect()
+  return (
+    rect.bottom >= 700 /* 700px to exclude footer height */ &&
+    rect.right >= 0 &&
+    rect.top <=
+      (window.innerHeight || document.documentElement.clientHeight) -
+        500 /* 500px after textarea enter viewport */ &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+window.addEventListener('scroll', () => {
+  if (
+    isElementInViewport(editPageTextArea) &&
+    editPageTextAreaHeight >= viewportHeight
+  ) {
+    markdownHelper.classList.add('fixed')
+  } else {
+    markdownHelper.classList.remove('fixed')
+  }
+})
+markdownHelperCross.addEventListener('click', event => {
+  event.preventDefault()
+  markdownHelper.classList.remove('fixed')
+})
