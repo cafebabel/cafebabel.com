71fb6892f0ee64f5603cede24ca22efa0aa8b0d2
David Larlet <david@larlet.fr>
2018-04-03 13:06:55 -0400

    Pertinent profiles articles/translations, fix #385
    And the ability to display the whole list of contributions on demand 💣

--- cafebabel/tests/test_profile.py
+++ cafebabel/tests/test_profile.py
@@ -14,20 +14,63 @@ def test_confirm_user_creates_default_profile(app):
     assert user.profile.name == 'Anonymous'
 
 
-def test_user_profile_has_list_of_published_articles_no_draft(client, article):
+def test_user_profile_has_button_to_full_list(client, article):
     response = client.get(f'/en/profile/{article.authors[0].id}/')
     assert response.status_code == HTTPStatus.OK
-    assert article.title not in response
+    assert 'See all articles and translations' in response
+
+
+def test_user_profile_full_has_no_button_to_list(client, article):
+    response = client.get(f'/en/profile/{article.authors[0].id}/?full')
+    assert response.status_code == HTTPStatus.OK
+    assert 'See all articles and translations' not in response
+
+
+def test_user_profile_has_list_of_published_articles_no_draft(
+        client, article, published_article):
+    response = client.get(f'/en/profile/{article.authors[0].id}/')
+    assert response.status_code == HTTPStatus.OK
+    assert article.detail_url not in response
+    assert published_article.detail_url in response
+
+
+def test_user_profile_has_list_of_published_translations_no_draft(
+        client, translation, published_translation):
+    response = client.get(f'/en/profile/{translation.authors[0].id}/')
+    assert response.status_code == HTTPStatus.OK
+    assert translation.detail_url not in response
+    assert published_translation.detail_url in response
 
 
 def test_author_profile_has_list_of_published_articles_and_drafts(
         client, user, article, published_article):
     login(client, user.email, 'password')
-    published_article.modify(authors=[user])
+    response = client.get(f'/en/profile/{article.authors[0].id}/')
+    assert response.status_code == HTTPStatus.OK
+    assert article.detail_url in response
+    assert published_article.detail_url in response
+
+
+def test_author_profile_has_list_of_published_translations_and_drafts(
+        client, user, translation, published_translation):
+    login(client, user.email, 'password')
+    response = client.get(f'/en/profile/{translation.authors[0].id}/')
+    assert response.status_code == HTTPStatus.OK
+    assert translation.detail_url in response
+    assert published_translation.detail_url in response
+
+
+def test_author_profile_has_list_of_its_translations_only(
+        client, user, user2, published_translation):
+    login(client, user.email, 'password')
+    assert published_translation.authors == [user]
+    published_translation.modify(translators=[user2])
     response = client.get(f'/en/profile/{user.id}/')
     assert response.status_code == HTTPStatus.OK
-    assert article.title in response
-    assert published_article.title in response
+    assert published_translation.detail_url not in response
+    response = client.get(f'/en/profile/{user2.id}/')
+    assert response.status_code == HTTPStatus.OK
+    assert published_translation.detail_url in response
 
 
 def test_editor_can_promote_user_as_editor(client, user, editor):
